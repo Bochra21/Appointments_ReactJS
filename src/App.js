bb231ad44a64968eeef6849dac5e58dbfd8d57c0
bochr <bochra.isitcom@gmail.com>
2024-02-22 23:18:15 +0100

    added login and theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 //import './App.css';
 import React from "react";
-import Login from "./pages/auth/Login";
+import Login from "./pages/auth/loginPage/Login";
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
 
+// defaultTheme
+import themes from './themes';
 
 function App() {
+
+  // const customization = useSelector((state) => state.customization);
+
   return (
     <>
+     <StyledEngineProvider injectFirst>
+      {/* <ThemeProvider theme={themes(customization)}> */}
+      <ThemeProvider  theme={themes}>
+        <CssBaseline />
       <Login/>
-   
+      </ThemeProvider>
+      </StyledEngineProvider>
     </>
   );
 }
